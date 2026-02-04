e4c731720a787743f95ca8752c01e5dc5db871eb
Kauo7420 <2673861652@qq.com>
2026-02-04 18:11:11 +0800

    Update plume.config.ts

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -91,55 +91,6 @@ export default defineThemeConfig({
       title: '博客',
       include: ['**/*.md'], // 包含所有 .md 文件
       exclude: ['**/*.snippet.md'] // 排除代码片段文件
-      postList: true, // 启用文章列表页
-      link: '/docs/blog/', // 列表页链接
-      linkPrefix: '/blog/', // 文章链接前缀
-      tags: true, // 启用标签页
-      tagsLink: '/blog/tags/', // 标签页链接
-      tagsTheme: 'colored', // 标签主题 colored|gray|brand
-      tagsText: '标签', // 标签页标题
-      archives: true, // 启用归档页
-      archivesLink: '/blog/archives/', // 归档页链接
-      archivesText: '归档', // 归档页标题
-      categories: true, // 启用分类页
-      categoriesLink: '/blog/categories/', // 分类页链接
-      categoriesText: '分类', // 分类页标题
-      categoriesExpand: 'deep', // 分类展开层级 number|'deep'
-      categoriesTransform: categories => categories, // 分类转换函数
-      autoFrontmatter: {
-        title: true, // 自动生成标题
-        createTime: true, // 自动生成创建时间
-        permalink: true, // 自动生成永久链接
-        transform: (data, context, locale) => { // 自定义转换
-          data.foo ??= 'foo'
-          return data
-        }
-      },
-      profile: {
-          avatar: '/avatar.jpg', // 头像路径
-          name: '卡沃Kauo', // 显示名称
-          description: '体温36.5°，但人生恒温-7°C', // 简介文本
-          circle: true, // 圆形头像
-          location: '湖南', // 所在地
-          organization: 'BlockVerse', // 所属组织
-          layout: 'right', // 布局位置 left|right
-      },
-      social: [
-        // 使用 iconify name
-          { icon: 'github', link: 'https://github.com/Kauo7420' },
-          { icon: 'bilibili ', link: 'https://space.bilibili.com/375148183' },
-      ],
-      meta: {
-          tags: true, // 是否显示标签
-          /**
-           * 是否显示创建时间，或设置时间格式
-           * - 'short': 显示为 `2022-01-01`，默认
-           * - 'long': 显示为 `2022-01-01 00:00:00`
-           */
-          createTime: true, // boolean | 'short' | 'long'
-          readingTime: true, // 是否显示阅读时间估算
-          wordCount: true, // 是否显示字数统计
-      }
     }
   ]
 
